80f52bcf243b37f4d2d4b1b15ee55382d1f33e67
Artem Tikhomirov <artem@tikhomirovs.ru>
2011-11-21 16:02:55 +0400

    First commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JetBrains.Annotations")]
+[assembly: AssemblyDescription("JetBrains ReSharper default annotations implementation.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JetBrains")]
+[assembly: AssemblyProduct("JetBrains.Annotations")]
+[assembly: AssemblyCopyright("Copyright © JetBrains 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("6.1.0.0")]
+[assembly: AssemblyFileVersion("6.1.0.0")]
